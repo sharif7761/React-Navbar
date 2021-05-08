b00518d4508a21fde1d80e5d9b5d3617b6270f54
sharif7761 <sharif7761@gmail.com>
2021-05-09 01:52:35 +0600

    navbar design setup

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,45 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return (
+      <nav>
+        <div class='nav-center'>
+          <div class='nav-header'>
+            <img src={logo} alt='logo'/>
+            <button class='nav-toggle'>
+              <FaBars />
+            </button>
+          </div>
+          <div class='links-container show-container'>
+            <ul class='links'>
+              <li>
+                <a href='#'>Home</a>
+              </li>
+              <li>
+                <a href='#'>About</a>
+              </li>
+              <li>
+                <a href='#'>Contact</a>
+              </li>
+              <li>
+                <a href='#'>Products</a>
+              </li>
+            </ul>
+          </div>
+          <ul class='social-icons'>
+            <li>
+              <a href='#'><FaTwitter /></a>
+            </li>
+            <li>
+              <a href='#'><FaTwitter /></a>
+            </li>
+            <li>
+              <a href='#'><FaTwitter /></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+  )
 }
 
 export default Navbar
